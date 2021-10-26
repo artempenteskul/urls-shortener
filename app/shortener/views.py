1d9b0cc6a3b50e140b6130dbb10c45cc3641c12c
Artem Penteskul <67336873+tommyfromche@users.noreply.github.com>
2021-10-26 17:39:04 +0300

    update-views

--- app/shortener/views.py
+++ app/shortener/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from rest_framework import generics
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Url
@@ -19,18 +20,16 @@ class UrlListView(generics.ListAPIView):
 
 
 class UrlShortener(generics.CreateAPIView):
-    """
-        url shortener
-    """
     serializer_class = UrlSerializer
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
-        url = Url.objects.filter(url=kwargs['full_url'], user=request.user).first()
+        url = request.data['url']
+        url = Url.objects.filter(url=request.data['url'])
         if url:
             short_url = url.short_url
-            return Response(short_url)
-        serializer = UrlSerializer(data={'url': kwargs['full_url']})
+            return Response(short_url, status=status.HTTP_201_CREATED)
+        serializer = UrlSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             url = serializer.save(user=request.user, scheme=request.scheme, hostname=request.get_host())
             short_url = url.short_url
@@ -51,8 +50,6 @@ class UrlCountView(generics.ListAPIView):
     """
         counts the number of distinct urls
     """
-    renderer_classes = [JSONRenderer]
-
     def get(self, request, **kwargs):
         urls_count = Url.objects.count()
         content = {'urls_count': urls_count}
@@ -63,8 +60,6 @@ class UrlPopularView(generics.ListAPIView):
     """
         returns ten most popular shortened urls
     """
-    renderer_classes = [JSONRenderer]
-
     def get(self, request, **kwargs):
         urls_popular = Url.objects.values('url').annotate(total=Count('id', distinct=True)).order_by('-total')[:10]
         content = {'urls_popular': urls_popular}
